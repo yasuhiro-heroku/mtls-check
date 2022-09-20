eb614539ff1b7eaf2745e8ac0132e4ce9f2fc6ac
Yasuhiro Miyake <47148542+yasuhiro-heroku@users.noreply.github.com>
2022-09-20 14:55:31 +0800

    initial commit

---
+++ connect.js
@@ -0,0 +1,19 @@
+const { Client } = require('pg');
+// console.log('The value of PORT is:', process.env.DATABASE_URL);
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
